36eaf6c436174a55df4b09a8a583a2d3e2e9edea
Pijush Gupta <pijush@live.com>
2023-06-27 12:27:28 +0530

    adding conditional setting option for global css

--- core/enqueue.php
+++ core/enqueue.php
@@ -5,12 +5,30 @@ if (!defined('ABSPATH')) exit; // Exit if accessed directly
 class Enqueue{
     public static function frontend(){
         add_action('wp_enqueue_scripts',array('tailwindJIT\Enqueue','start'));
-        add_action( 'wp_enqueue_scripts', array('tailwindJIT\Enqueue','deregister'), 100 );
-    }
 
+        /**
+         * checking if `Disable WordPress global style` is enabled or not 
+         * This to remove wordpress global css 
+         */
+        if(get_option('tailwindJITDisableGlobalCss',false) != false){
+            add_action( 'wp_enqueue_scripts', array('tailwindJIT\Enqueue','deregister'), 100 );
+        }
+    }
+    
+    /**
+     * adding tailwind css to the frontend
+     *
+     * @return void
+     */
     public static function start(){
         wp_enqueue_style('tailwindCss',plugins_url('', __FILE__).'/../dist/main.css',array(),'1','all');
     }
+
+    /**
+     * removing wordpress global css
+     *
+     * @return void
+     */
     public static function deregister(){
         wp_dequeue_style('global-styles'); 
         wp_deregister_style('global-styles'); 
